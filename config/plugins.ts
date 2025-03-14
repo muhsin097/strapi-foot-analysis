4a0219e1a8315f3bc08fc3a5d4c106c76a65cdea
Muhsin Musthafa <muhsin.musthafa@beta-blink.com>
2025-03-14 16:24:32 +0530

    fix 3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,9 @@
-module.exports = ({ env }) => ({
+import crypto from "crypto";
+
+export default ({ env }) => ({
   "users-permissions": {
     config: {
-      jwtSecret: env("JWT_SECRET"),
+      jwtSecret: env("JWT_SECRET", crypto.randomBytes(16).toString("base64")),
     },
   },
 });
